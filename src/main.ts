7e38440bb0f2c6a0b844859a066f953d3f1ab55c
Stanislavsky2024 <filepark.stas@gmail.com>
2025-12-10 16:17:54 +0900

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import cookieParser from 'cookie-parser';
 import { ZodValidationPipe } from 'nestjs-zod';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:5173'],
+      credentials: true,
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      methods: ['POST', 'GET'],
+    },
+  });
 
   app.use(cookieParser());
   app.useGlobalPipes(new ZodValidationPipe());
